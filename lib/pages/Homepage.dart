c3fa6562859b327dce12f90c3b41c5631f0676bf
Afiq Salleh <afiqsalleh97@gmail.com>
2022-08-08 15:50:00 +0800

    Updating the models, details, services, states and widgets for the project.

---
+++ lib/pages/Homepage.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:food_recipe_project/pages/foodRecipe_detail.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required String title}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late TextEditingController _controller;
+  bool _isFoodRecipeNameEmpty = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = TextEditingController();
+
+    _controller.addListener(() {
+      setState(() => _isFoodRecipeNameEmpty = _controller.text.isEmpty);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Food Recipe'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Please enter food recipe name',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  hintText: 'Recipe',
+                  label: Text('Food Recipe Name'),
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Search'),
+              onPressed: _isFoodRecipeNameEmpty
+                  ? null
+                  : () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => FoodRecipeDetail(
+                            foodName: _controller.text,
+                          ),
+                        ),
+                      );
+                    },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
